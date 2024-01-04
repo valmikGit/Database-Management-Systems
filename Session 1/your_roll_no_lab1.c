fe74652911502b1d1ddeff0007855d1c8c4e1a62
valmikGit <quantumcomputingiiitb@gmail.com>
2024-01-04 19:20:23 +0530

    Made the DBMS repository.

---
+++ Session 1/your_roll_no_lab1.c
@@ -0,0 +1,41 @@
+# include <stdio.h> 
+
+struct student {
+  int rollnum;
+  char name[30];
+  int age;
+};
+
+int save_num_text( char *filename ) {
+//write your code
+}
+
+int read_num_text( char *filename ) {
+  //write your code
+}
+
+int save_struct_text( char *filename ) {
+  //write your code
+}
+
+int read_struct_text( char *filename ) {
+//write your code
+}
+
+
+int save_num_binary( char *filename ) {
+//write your code
+}
+
+int read_num_binary( char *filename ) {
+ //write your code
+}
+
+int save_struct_binary( char *filename ) {
+  //write your code
+}
+
+int read_struct_binary( char *filename ) {
+ //write your code
+}
+
